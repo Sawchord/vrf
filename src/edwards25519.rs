20402ddcd629fb72ea920335f477025ba7532232
Leon Tan <leon.arian.tan@gmail.com>
2021-11-10 21:54:20 +0100

    Switch to using key pairs

--- src/edwards25519.rs
+++ src/edwards25519.rs
@@ -1,11 +1,11 @@
-use lucius_curves::edwards25519::{Edwards25519, PublicKey, SecretKey};
+use lucius_curves::edwards25519::{Edwards25519, KeyPair, PublicKey};
 
 pub struct VrfProof {}
 
 impl crate::VrfProof for VrfProof {
     type Curve = Edwards25519;
 
-    fn generate(secret_key: &SecretKey, data: impl AsRef<[u8]>) -> (Self, crate::VfrHash) {
+    fn generate(key_pair: &KeyPair, data: impl AsRef<[u8]>) -> (Self, crate::VfrHash) {
         todo!()
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use lucius_curves::{Curve, PublicKey, SecretKey};
+use lucius_curves::{Curve, KeyPair, PublicKey};
 
 mod edwards25519;
 
@@ -7,7 +7,7 @@ pub type VfrHash = [u8; 32];
 pub trait VrfProof: Sized {
     type Curve: Curve;
 
-    fn generate(secret_key: &SecretKey<Self::Curve>, data: impl AsRef<[u8]>) -> (Self, VfrHash);
+    fn generate(key_pair: &KeyPair<Self::Curve>, data: impl AsRef<[u8]>) -> (Self, VfrHash);
 
     fn verify(&self, public_key: &PublicKey<Self::Curve>) -> Result<VfrHash, ()>;
 }
